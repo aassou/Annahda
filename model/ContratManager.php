a50f6e2e69e06216bb822fb07ae877d4703c288e
unknown <aassou.abdelilah@gmail.com>
2015-11-10 16:29:54 +0000

    Commit contrats-list.php

--- model/ContratManager.php
+++ model/ContratManager.php
@@ -212,7 +212,7 @@ class ContratManager{
 	
 	public function getContratsByIdProjetOnly($idProjet){
         $contrats = array();    
-        $query = $this->_db->prepare("SELECT * FROM t_contrat WHERE idProjet=:idProjet");
+        $query = $this->_db->prepare("SELECT * FROM t_contrat WHERE idProjet=:idProjet ORDER BY status ASC, dateCreation DESC");
         $query->bindValue(':idProjet', $idProjet);
         $query->execute();
         while($data = $query->fetch(PDO::FETCH_ASSOC)){
