311eed38e46662fd0f257580eb1185be02d26e37
unknown <aassou.abdelilah@gmail.com>
2015-11-10 16:02:34 +0000

    Commit contrat-list.php

--- config.php
+++ config.php
@@ -1,2 +1,2 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=annahda', 'root', '');
+$pdo = new PDO('mysql:host=mysql.hostinger.co.uk;dbname=u914861886_immo', 'u914861886_abdo', 'ImmoERP2015DB');
