70ef8bb9da203174c86eda7fd360eed3c6d71eaa
unknown <aassou.abdelilah@gmail.com>
2016-12-29 16:04:31 +0000

    Responsive Design - Date : 29/12/2016

--- include/top-menu.php
+++ include/top-menu.php
@@ -88,9 +88,9 @@
 				<i class="icon-user"></i>
 				</a>
 				<ul class="dropdown-menu">
-					<li><a href="user-profil.php"><i class="icon-user"></i><?= $_SESSION['userMerlaTrav']->login(); ?></a></li>
+					<li><a href="user-profil.php"><i class="icon-user"></i>&nbsp;<?= $_SESSION['userMerlaTrav']->login(); ?></a></li>
 					<li class="divider"></li>
-					<li><a href="logout.php"><i class="icon-key"></i> Se déconnecter</a></li>
+					<li><a href="logout.php"><i class="icon-key"></i>&nbsp;Se déconnecter</a></li>
 				</ul>
 			</li>
 			<!-- END USER LOGIN DROPDOWN -->
