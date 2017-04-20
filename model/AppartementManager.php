55d92f7b890ba1ec955bb759406eaa4615528183
unknown <aassou.abdelilah@gmail.com>
2017-04-20 14:35:02 +0100

    New Updates - Date : 20/04/2017

--- model/AppartementManager.php
+++ model/AppartementManager.php
@@ -94,8 +94,7 @@ class AppartementManager{
     //This method is used when a contract is reselled, so we need to mention the reselling price
     //without touching the real price
     public function updateMontantRevente($montantRevente, $idBien){
-        $query = $this->_db->prepare('
-        UPDATE t_appartement SET montantRevente=:montantRevente WHERE id=:idAppartement') 
+        $query = $this->_db->prepare('UPDATE t_appartement SET montantRevente=:montantRevente WHERE id=:idAppartement') 
         or die(print_r($this->_db->errorInfo()));
         $query->bindValue(':idAppartement', $idBien);
         $query->bindValue(':montantRevente', $montantRevente);
