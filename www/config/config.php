df1d199ac4ad5d8f651e70844b96c5ffb4d069e9
abdel <aassou.abdelilah@gmail.com>
2018-12-02 21:25:22 +0100

    Fix issues

--- www/config/config.php
+++ www/config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:host=db;dbname=annahda', 'root', 'test');
+    $pdo = new PDO('mysql:host=db;dbname=db', 'user', 'pass');
     //openssl encryption params
-    $password = "iR0nM@N2017!?KOreVoNick";
-    $method = "aes128";
-    $iv = "69kjg23423L@cEv7";
+    $password = "";
+    $method = "";
+    $iv = "";
     //numbers and floats camouflage
-    $mutation = 123456789;
+    $mutation = 123456;
 }
 catch(Exception $e){
     die(sprintf('Can`t connect to database: %s', $e->getMessage()));
