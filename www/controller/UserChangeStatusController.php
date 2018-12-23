d19bb6b8f2e14f3a05ed9b713be9e59365e16c1a
aassou <aassou.abdelilah@gmail.com>
2018-12-23 21:12:52 +0100

    Fix broken links for User

--- www/controller/UserChangeStatusController.php
+++ www/controller/UserChangeStatusController.php
@@ -10,7 +10,7 @@ function classLoad ($myClass) {
     }
 }
 spl_autoload_register("classLoad");
-include("../config.php");
+include("../config/config.php");
 //classes loading end
 session_start();
 
